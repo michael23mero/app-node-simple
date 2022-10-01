91dc14e0f345e293e20ff4a2d215bb77b81bf783
michael23mero <maicol23mero@gmail.com>
2022-09-30 21:11:16 -0500

    integracion de doble autenticacion

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,4 +1,6 @@
 const rutas = require('express').Router()
+const speakeasy = require('speakeasy')
+const QRCode = require('qrcode')
 
 rutas.get('/', (req, res) =>{
     res.render('index', { title : 'Inicio'})
@@ -12,6 +14,25 @@ rutas.post('/login', (req, res) => {
     const { ...data }  = req.body
     console.log(data)
     res.redirect('/')
+})
+
+rutas.get('/login2', (req, res) => {
+    const secret = speakeasy.generateSecret()
+    QRCode.toDataURL(secret.otpauth_url, (err, dataUrl) => {
+        if(err || !dataUrl){
+            return reject(err)
+        }
+        res.render('login2', {title: 'login2', secret: secret.base32, qrCode: dataUrl}) 
+    })
+})
+
+rutas.post('/login2', (req, res) => {
+    console.log(req.body)
+    const { token, secret } = req.body;
+    const validation = speakeasy.totp.verify({
+        secret, encoding: 'base32', token
+    })
+    res.json(validation)
 })
 
 module.exports = rutas
